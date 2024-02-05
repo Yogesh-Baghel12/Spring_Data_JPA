13b912fabe45a28f0889e008666d741b98d1d27c
Yogesh <yashbaghel613@gmail.com>
2024-02-04 22:53:50 -0800

    MAke simple CRUD operation using API

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -5,21 +5,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.luv2code.cruddemo.dao.StudentDAO;
+
+import com.luv2code.cruddemo.dao.StudentRepository;
 
 @SpringBootApplication
 public class CruddemoApplication {
+
+	private StudentRepository studentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+	public CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
 		return runner->{
-			createStudent(studentDAO);
+			createStudent(studentRepository);
 		};
 	}
-	private void createStudent(StudentDAO studentDAO) {
+	private void createStudent(StudentRepository studentRepository) {
 		//create student object
 		
 	}
